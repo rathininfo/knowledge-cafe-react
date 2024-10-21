710bd97f782c2257597c42361947f91de3f0c9eb
rathininfo <rathininfo7@gmail.com>
2024-10-21 23:56:21 +0600

    setup tailwind css in react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='bg-red-300 text-center text-white  font-bold'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
